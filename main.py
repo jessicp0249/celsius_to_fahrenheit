8a9b265475323de0b1c00304a3e6e0102ef8192c
jessicp0249 <42855762+jessicp0249@users.noreply.github.com>
2019-09-10 15:54:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,8 @@
 # Email: jessicp0249@student.vvc.edu
 # Created: 10 Sep 2019
 
+# Get temperature in Celsius from user
 temp_C = float(input('Enter temperature in Celsius:\n'))
+
+# Print converted temperature
 print('%0.2f degrees Fahrenheit' % (temp_C * 9 / 5 + 32))
